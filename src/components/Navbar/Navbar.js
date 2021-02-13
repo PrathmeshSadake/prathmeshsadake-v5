86481e54b7ee12806746223352f455aa3fd45048
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-14 00:54:29 +0530

    Created NavBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,43 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import mainLogo from "../../assets/main-logo.svg";
 
 import "./navbar.scss";
 
 export default function NavigationBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar
+      className="custom-padding"
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      data-aos="zoom-out"
+      data-aos-duration="300"
+      data-aos-delay="300"
+    >
+      <Navbar.Brand href="#home">
+        <Link to="/">
+          <img src={mainLogo} className="main-logo" alt="main logo" />
+        </Link>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav variant="pills" defaultActiveKey="link-1" className="mr-auto">
           <Nav.Item>
-            <Nav.Link eventKey="link-1" href="#home">
-              home
+            <Nav.Link eventKey="link-1" href="#">
+              <Link to="/">Home</Link>
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link eventKey="link-2" href="#projects">
-              projects
+              <Link to="/projects">projects</Link>
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link eventKey="link-3" href="#contact">
-              contact
+              <Link to="/contact">contact</Link>
             </Nav.Link>
           </Nav.Item>
         </Nav>
