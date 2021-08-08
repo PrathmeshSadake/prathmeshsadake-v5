dca3ca44f3028ad9d82b46184177421795ad8dac
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-08-09 01:51:41 +0530

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,6 +6,35 @@ import Project from "../components/Project/Project";
 export default function Projects() {
   const projects = [
     {
+      title: "Studio Seven",
+      subtitle: "Clothing Store",
+      description:
+        "Clothing store website made with ReactJs, Redux and Sass. Has features fetching data from google firestore database and user signin and signup using firebase auth.",
+      imageUrl: process.env.PUBLIC_URL + "/images/projects/studioseven.png",
+      technologies: ["React", "react-redux", "Sass", "Firebase", "react-router-dom"],
+      githubUrl:
+        "https://www.github.com/PrathmeshSadake/StudioSeven",
+    },
+    {
+      title: "Portfolio Website",
+      subtitle: "Personal Website",
+      description:
+        "Personal portfolio website made with React.js to showcase about us, projects to employers and other members to let them know why you will fit into their organization. There is also a contact form which is made using email-js api. ",
+      imageUrl: process.env.PUBLIC_URL + "/images/projects/portfolio.png",
+      technologies: ["React", "react-bootstrap", "SCSS", "AOS-Animations"],
+      githubUrl:
+        "https://www.github.com/PrathmeshSadake/prathmeshsadake.github.io",
+    },
+    {
+      title: "Unsplashy",
+      subtitle: "Free stock images React Application",
+      description:
+        "An image gallery which fetches images from unsplash api. It has some awesome features like searching for images and infinite scrolling in which images keeps on loading as user scrolls",
+      imageUrl: process.env.PUBLIC_URL + "/images/projects/unsplashy.png",
+      technologies: ["React", "react-bootstrap", "css", "unsplash-api"],
+      githubUrl: "https://www.github.com/PrathmeshSadake/unsplashy",
+    },
+    {
       title: "Newscast",
       subtitle: "Flutter News Application",
       description:
@@ -28,25 +57,7 @@ export default function Projects() {
       technologies: ["Flutter", "Dart", "GitHub jobs Api"],
       githubUrl: "https://www.github.com/PrathmeshSadake/devjobs",
     },
-    {
-      title: "Portfolio Website",
-      subtitle: "Personal Website",
-      description:
-        "Personal portfolio website made with React.js to showcase about us, projects to employers and other members to let them know why you will fit into their organization. There is also a contact form which is made using email-js api. ",
-      imageUrl: process.env.PUBLIC_URL + "/images/projects/portfolio.png",
-      technologies: ["React", "react-bootstrap", "SCSS", "AOS-Animations"],
-      githubUrl:
-        "https://www.github.com/PrathmeshSadake/prathmeshsadake.github.io",
-    },
-    {
-      title: "Unsplashy",
-      subtitle: "Free stock images React Application",
-      description:
-        "An image gallery which fetches images from unsplash api. It has some awesome features like searching for images and infinite scrolling in which images keeps on loading as user scrolls",
-      imageUrl: process.env.PUBLIC_URL + "/images/projects/unsplashy.png",
-      technologies: ["React", "react-bootstrap", "css", "unsplash-api"],
-      githubUrl: "https://www.github.com/PrathmeshSadake/unsplashy",
-    },
+    
   ];
   return (
     <div>
