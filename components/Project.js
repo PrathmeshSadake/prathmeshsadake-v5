685999e8a0d3ca9035490bb254187c1742ea795e
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-01-13 18:34:37 +0530

    Added project component from codepen

---
+++ components/Project.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Project = () => {
+  return (
+    <div class="project-container">
+      <div class="project">
+        <div class="project-content">
+          <div class="project-label">Game of Thrones</div>
+          <h4 class="project-title">A Clash of Kings</h4>
+          <div class="project-details">
+            <p>
+              Lorem ipsum dolor amet you probably haven't heard of them bitters
+              selvage listicle heirloom. Locavore kombucha street art ennui
+              90's, organic food truck hell of seitan skateboard literally
+              hexagon fixie next level. Lomo salvia yuccie hella roof party echo
+              park vegan four dollar toast cred.
+            </p>
+            <ul>
+              <li>Shadow</li>
+              <li>Demon</li>
+              <li>Baby</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="project-img">
+          <img
+            src="https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
