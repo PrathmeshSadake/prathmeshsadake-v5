9392b5fb545e0d7b90024aa9322bd41e07e7bfad
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-06-03 16:08:16 +0530

    Added custom colors✅

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        purple: '#3f3cbb',
+        midnight: '#121063',
+        metal: '#565584',
+        tahiti: '#3ab7bf',
+        silver: '#ecebff',
+        'bubble-gum': '#ff77e9',
+        bermuda: '#78dcca',
+        primary: '#011214',
+        secondary: '#04AB5C',
+      },
+    },
   },
   plugins: [],
 };
