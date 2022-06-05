009bac1881ade64d18726d48549c6a50563e0e89
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-06-05 13:08:31 +0530

    installed react-scroll-section✅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// next.config.js
+const withTM = require('next-transpile-modules')(['react-scroll-section']); // pass the modules you would like to see transpiled
+
+module.exports = withTM({});
