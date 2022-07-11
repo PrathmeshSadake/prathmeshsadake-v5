544a06b92c5f180bcedc8ea9167a4aea31304429
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-07-12 02:06:02 +0530

    noreferrer error fixed

--- pages/resume.js
+++ pages/resume.js
@@ -12,8 +12,7 @@ export default function PDF() {
         className='max-w-min mx-auto py-3 px-8 text-base justify-between  hover:bg-gray-800 font-medium leading-none text-white bg-gray-700 flex items-center transition-colors'
         href={`https://drive.google.com/uc?export=download&id=1M_UiFPGf8zJlg4B8qv4B9AI76EWFMPIS`}
         target='_blank'
-        noopener
-        noreferrer
+        rel='noreferrer'
         download={true}
       >
         Download
