4bd597c2081596e9b6c207f085943eaf58be2f1b
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-07-24 13:21:12 +0530

    Added container types

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,6 @@
 import Link from "next/link";
 import clsx from "clsx";
 
-const variantStyles = {
-  primary:
-    "bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70",
-  secondary:
-    "bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70",
-};
-
 export function Button({
   variant,
   className,
@@ -20,8 +13,10 @@ export function Button({
 }) {
   className = clsx(
     "inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none",
-    variantStyles[variant],
-    className
+    className,
+    variant === "secondary"
+      ? "bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70"
+      : "bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70"
   );
 
   return href ? (
--- components/Container.tsx
+++ components/Container.tsx
@@ -1,48 +1,48 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, ReactNode, HTMLAttributes } from "react";
 import clsx from "clsx";
 
-const OuterContainer = forwardRef(function OuterContainer({
-  className,
-  children,
-  ...props
-}: {
-  className: string;
-  children: React.ReactNode;
-}) {
-  return (
-    <div className={clsx("sm:px-8", className)} {...props}>
-      <div className='mx-auto max-w-7xl lg:px-8'>{children}</div>
-    </div>
-  );
-});
+interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
 
-const InnerContainer = forwardRef(function InnerContainer({
-  className,
-  children,
-  ...props
-}: {
-  className?: string;
-  children: React.ReactNode;
-}) {
-  return (
-    <div
-      className={clsx("relative px-4 sm:px-8 lg:px-12", className)}
-      {...props}
-    >
-      <div className='mx-auto max-w-2xl lg:max-w-5xl'>{children}</div>
-    </div>
-  );
-});
+const OuterContainer = forwardRef<HTMLDivElement, ContainerProps>(
+  function OuterContainer({ className, children, ...props }, ref) {
+    return (
+      <div ref={ref} className={clsx("sm:px-8", className)} {...props}>
+        <div className='mx-auto max-w-7xl lg:px-8'>{children}</div>
+      </div>
+    );
+  }
+);
 
-export const Container = forwardRef(function Container({
-  children,
-  ...props
-}: {
-  children: React.ReactNode;
-  className: string;
-}) {
+const InnerContainer = forwardRef<HTMLDivElement, ContainerProps>(
+  function InnerContainer({ className, children, ...props }, ref) {
+    return (
+      <div
+        ref={ref}
+        className={clsx("relative px-4 sm:px-8 lg:px-12", className)}
+        {...props}
+      >
+        <div className='mx-auto max-w-2xl lg:max-w-5xl'>{children}</div>
+      </div>
+    );
+  }
+);
+
+interface ContainerComponent
+  extends React.ForwardRefExoticComponent<
+    ContainerProps & React.RefAttributes<HTMLDivElement>
+  > {
+  Outer?: typeof OuterContainer;
+  Inner?: typeof InnerContainer;
+}
+
+export const Container: ContainerComponent = forwardRef<
+  HTMLDivElement,
+  ContainerProps
+>(function Container({ children, ...props }, ref) {
   return (
-    <OuterContainer {...props}>
+    <OuterContainer ref={ref} {...props}>
       <InnerContainer>{children}</InnerContainer>
     </OuterContainer>
   );
