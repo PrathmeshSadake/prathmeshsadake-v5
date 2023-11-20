aa006a41db9fab5aa1ceb329f9cf576f7c2b7539
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-11-20 12:00:10 +0530

    vite react setup

--- components/NextThemeProvider.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { ThemeProvider } from "next-themes";
-
-const NextThemeProvider = ({ children }) => {
-  return <ThemeProvider attribute='class'>{children}</ThemeProvider>;
-};
-
-export default NextThemeProvider;
