d8c7b02f76ce284e43700bca30078caa418970ec
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-11-23 15:55:24 +0530

    Made ui changes to footer and removed unwanted projects

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,20 +7,22 @@ const Footer = () => {
     <footer className='footer font-poppins'>
       {/* <hr className='border-slate-200' /> */}
 
-      <div className='footer-container'>
+      <div className='footer-container mt-4'>
         <p>
-          © 2023 <strong>Prathmesh Sadake</strong>. All rights reserved.
+          © 2024 <strong>Prathmesh Sadake</strong>. All rights reserved.
         </p>
 
-        <div className='flex gap-3 justify-center items-center'>
+        <div className='w-full md:w-fit flex text-center gap-3 justify-center items-center'>
           {socialLinks.map((link) => (
-            <Link key={link.name} to={link.link} target='_blank'>
-              <img
-                src={link.iconUrl}
-                alt={link.name}
-                className='w-6 h-6 object-contain'
-              />
-            </Link>
+            <div key={link.name}>
+              <Link to={link.link} target='_blank'>
+                <img
+                  src={link.iconUrl}
+                  alt={link.name}
+                  className='w-6 h-6 object-contain'
+                />
+              </Link>
+            </div>
           ))}
         </div>
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -64,16 +64,6 @@ export const skills = [
     type: "Database",
   },
   {
-    imageUrl: mongodb,
-    name: "MySQL",
-    type: "Database",
-  },
-  {
-    imageUrl: mui,
-    name: "Material-UI",
-    type: "Frontend",
-  },
-  {
     imageUrl: nextjs,
     name: "Next.js",
     type: "Frontend",
@@ -109,11 +99,6 @@ export const skills = [
     type: "DevOps",
   },
 
-  {
-    imageUrl: grafana,
-    name: "Grafana",
-    type: "DevOps",
-  },
   {
     imageUrl: jenkins,
     name: "Jenkins",
@@ -212,47 +197,47 @@ export const projects = [
     theme: "btn-back-red",
     name: "Quill",
     description:
-      "Complete AI SaaS Platform with Next.js 13, React, Prisma, tRPC, Tailwind CSS enabling users to upload a PDF document and chat with AI assistant about the document.",
+      "Complete AI SaaS Platform with Next.js 13, React, Prisma, tRPC, Tailwind CSS enabling users to upload a PDF document and chat with AI assistant about the document with free tier and paid subscriptions.",
     link: "https://github.com/prathmeshsadake/quill",
   },
   {
     iconUrl: threads,
     theme: "btn-back-green",
-    name: "Full Stack Threads Clone",
+    name: "Scuttle",
     description:
-      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-    link: "https://github.com/adrianhajdin/threads",
+      "Marketplace of high-quality digital assets across various categories, with features like admin verification for quality products. It also has file uploading, CMS integration, and role based authentication (Buyer and Seller).",
+    link: "https://github.com/PrathmeshSadake/scuttle",
   },
-  {
-    iconUrl: car,
-    theme: "btn-back-blue",
-    name: "Car Finding App",
-    description:
-      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
-    link: "https://github.com/adrianhajdin/project_next13_car_showcase",
-  },
-  {
-    iconUrl: snapgram,
-    theme: "btn-back-pink",
-    name: "Full Stack Instagram Clone",
-    description:
-      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-    link: "https://github.com/adrianhajdin/social_media_app",
-  },
-  {
-    iconUrl: estate,
-    theme: "btn-back-black",
-    name: "Real-Estate Application",
-    description:
-      "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
-    link: "https://github.com/adrianhajdin/projects_realestate",
-  },
-  {
-    iconUrl: summiz,
-    theme: "btn-back-yellow",
-    name: "AI Summarizer Application",
-    description:
-      "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
-    link: "https://github.com/adrianhajdin/project_ai_summarizer",
-  },
+  // {
+  //   iconUrl: car,
+  //   theme: "btn-back-blue",
+  //   name: "Car Finding App",
+  //   description:
+  //     "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
+  //   link: "https://github.com/adrianhajdin/project_next13_car_showcase",
+  // },
+  // {
+  //   iconUrl: snapgram,
+  //   theme: "btn-back-pink",
+  //   name: "Full Stack Instagram Clone",
+  //   description:
+  //     "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
+  //   link: "https://github.com/adrianhajdin/social_media_app",
+  // },
+  // {
+  //   iconUrl: estate,
+  //   theme: "btn-back-black",
+  //   name: "Real-Estate Application",
+  //   description:
+  //     "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
+  //   link: "https://github.com/adrianhajdin/projects_realestate",
+  // },
+  // {
+  //   iconUrl: summiz,
+  //   theme: "btn-back-yellow",
+  //   name: "AI Summarizer Application",
+  //   description:
+  //     "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
+  //   link: "https://github.com/adrianhajdin/project_ai_summarizer",
+  // },
 ];
